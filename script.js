0bf2de0d1e7bb2151f8bec08cc26526aac036231
Ayush Katiyar <ayush1270570@gmail.com>
2025-05-03 17:57:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+var typed = new Typed("#element", {
+  strings: ["Web Developer", "Designer", "Freelancer","app developer","problem solver"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true,
+});
